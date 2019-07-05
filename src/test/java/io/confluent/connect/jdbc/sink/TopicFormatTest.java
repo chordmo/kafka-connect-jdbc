b8d02d6cc52e1fd573a30968a262372547c207ab
m <m@mdeMacBook-Pro.local>
2019-07-05 14:47:50 +0800

    test

--- src/test/java/io/confluent/connect/jdbc/sink/TopicFormatTest.java
+++ src/test/java/io/confluent/connect/jdbc/sink/TopicFormatTest.java
@@ -10,7 +10,7 @@ public class TopicFormatTest {
     @Test
     public void replace() {
         final String tableName = "table1".replace("${topic}", "yz-table1");
-        final String tableName2 = "yz-table2".replace("yz-", "");
+        final String tableName2 = "yz-tabyz-le2yz-".replace("yz-{0}", "");
         System.out.println(tableName);
         System.out.println(tableName2);
     }
